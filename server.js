02b312882c59f89c17ca309b0faad3dddbdb867d
CharlyJose <charleyjoz2011@gmail.com>
2017-02-19 23:37:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+// Creating articleOne object
+
+var articleOne = {
+    title: 'Article One | Charly Jose',
+    heading: ' Article One',
+    date: 'Feb 19, 2017',
+    content:`
+            <p>This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. 
+            </p>
+            <p>This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article.  
+            </p>
+            <p>This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article. This is the content for the first article.
+            </p>`
+};
+
+// Function to manipulate the articleOne object
+
+function createtemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    // String variable
+    
+    var htmlTemplate =`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>${title}</title>
+            <meta charset = "utf-8"/> 
+            <link href="/ui/style.css" rel="stylesheet" />
+             <meta name="viewport" content="width=device-width, intial-scale=1" />
+        </head>
+        <body>
+            <div>
+                <a href="/">Home</a>
+             </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+             <div>
+                 ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </body>
+     </html>
+    `;
+    return htmlTemplate;   // Returning the string file 
+
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTempate(articleOne));  //sending back a string file
 });
 
 app.get('/Article-two', function(req,res){
